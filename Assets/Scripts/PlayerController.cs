e31253eeca03e399e6a3086a177cfb05c708108e
Armin Shahnami <shahnami.armin@gmail.com>
2024-10-07 18:57:46 +0100

    Change player Rigidbody to CharacterController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
-    public float speed = 500.0f;
+    public Vector3 CharacterVelocity;
+    public float speed = 10f;
+    private CharacterController controller;
+  
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-
-        GetComponent<Rigidbody>().velocity = movement * speed * Time.fixedDeltaTime;
+        Vector3 targetVelocity = transform.TransformVector(movement) * speed;
+        CharacterVelocity = Vector3.Lerp(CharacterVelocity, targetVelocity, 10f * Time.deltaTime);
+        controller.Move(CharacterVelocity * Time.deltaTime);
     }
 }
