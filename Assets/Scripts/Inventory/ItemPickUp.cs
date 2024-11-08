44c813f190cfc0257830d440c015f718f7c9c970
Armin Shahnami <shahnami.armin@gmail.com>
2024-11-08 14:54:16 +0000

    Refactor inventory item data

--- Assets/Scripts/Inventory/ItemPickUp.cs
+++ Assets/Scripts/Inventory/ItemPickUp.cs
@@ -11,7 +11,7 @@ public class ItemPickUp : MonoBehaviour
     private float bounceSpeed = 3f;
     private Vector3 originalPosition;
 
-    public InventoryItemData itemData;
+    public ItemData itemData;
 
     private SphereCollider myCollider;
 
@@ -38,7 +38,7 @@ public class ItemPickUp : MonoBehaviour
         var inventory = other.transform.GetComponent<InventoryHolder>();
         if(!inventory) return;
 
-        if(itemData.Type == ItemType.Weapon){
+        if(itemData.Type == ItemType.WEAPON){
             if(inventory.WeaponInventorySystem.AddToInventory(itemData, 1)){
                 Destroy(gameObject);
             }
