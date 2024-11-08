e4e8a5b8d634bf1fdd2e660de41573479fc18797
Armin Shahnami <shahnami.armin@gmail.com>
2024-11-08 09:03:01 +0000

    Make dropped item rotate

--- Assets/Scripts/Inventory/ItemPickUp.cs
+++ Assets/Scripts/Inventory/ItemPickUp.cs
@@ -6,17 +6,33 @@ using UnityEngine;
 public class ItemPickUp : MonoBehaviour
 {
     public float PickUpRadius = 1f;
+
+    private float bounceHeight = 0.1f;
+    private float bounceSpeed = 3f;
+    private Vector3 originalPosition;
+
     public InventoryItemData itemData;
 
     private SphereCollider myCollider;
-
 
     private void Awake(){
         myCollider = GetComponent<SphereCollider>();
         myCollider.isTrigger = true;
         myCollider.radius = PickUpRadius;
+
     }
 
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 40 * Time.deltaTime, 0); // Rotate the item
+        float newY = originalPosition.y + Mathf.Sin(Time.time * bounceSpeed) * bounceHeight; // Move the item up and down
+        transform.position = new Vector3(originalPosition.x, newY, originalPosition.z); // Set the new position
+    }
 
     private void OnTriggerEnter(Collider other) {
         var inventory = other.transform.GetComponent<InventoryHolder>();
