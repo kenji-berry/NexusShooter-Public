aef594b16caeb2b52f7e589cb68302d3f89d4a30
kenji-berry <kenji.k.berry@gmail.com>
2024-11-30 22:43:58 +0000

    Change Cost text, Made Tiers work

--- Assets/Scripts/Player/Skill Tree/Upgrades/Upgrade.cs
+++ Assets/Scripts/Player/Skill Tree/Upgrades/Upgrade.cs
@@ -2,11 +2,18 @@ public abstract class Upgrade
 {
     public string name;
     public int skillPointsCost;
+    public bool IsPurchased { get; private set; }
 
     public Upgrade(string name, int skillPointsCost)
     {
         this.name = name;
         this.skillPointsCost = skillPointsCost;
+        this.IsPurchased = false; // Initialize as not purchased
+    }
+
+    public void Purchase()
+    {
+        IsPurchased = true;
     }
 
     public abstract void ApplyUpgrade();
