1c963ce43652468412416141b34b24d0c7c7322f
IvanSeagull <ivan.seagull.k@gmail.com>
2024-12-03 14:35:58 +0000

    New guns

---
+++ Assets/Scripts/Weapon System/AK47.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK47 : Gun
+{
+    public override void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, Mathf.Infinity, shootableMask)) // Check if the raycast hits an enemy
+        { 
+            EnemyHealthController enemyHealthController = hit.transform.GetComponent<EnemyHealthController>();
+            if (enemyHealthController != null)
+            {
+                soundController.Play(soundController.hit);
+                enemyHealthController.TakeDamage(gunData.damage);
+            }
+        }
+    }
+}
