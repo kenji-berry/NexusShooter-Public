b2654a9eef12ee308dbcc17fb4002d39bc97ebfc
Armin Shahnami <shahnami.armin@gmail.com>
2024-12-29 20:02:36 +0000

    Update armour and weapon pickups

--- Assets/_Scripts/Inventory/Weapon Inventory/WeaponPickUp.cs
+++ Assets/_Scripts/Inventory/Weapon Inventory/WeaponPickUp.cs
@@ -9,10 +9,6 @@ public class WeaponPickUp : MonoBehaviour
 
     public float PickUpRadius = 1f;
 
-    private float bounceHeight = 0.1f;
-    private float bounceSpeed = 3f;
-    private Vector3 originalPosition;
-
     private SphereCollider myCollider;
 
     public WeaponsManager weaponsManager;
@@ -23,18 +19,9 @@ public class WeaponPickUp : MonoBehaviour
         myCollider.radius = PickUpRadius;
     }
 
-    void Start()
-    {
-        originalPosition = transform.position;
-    }
-
     void Update()
     {
-        /* Rotate and bob item up and down
-         */
         transform.Rotate(0, 40 * Time.deltaTime, 0);
-        float newY = originalPosition.y + Mathf.Sin(Time.time * bounceSpeed) * bounceHeight;
-        transform.position = new Vector3(originalPosition.x, newY, originalPosition.z);
     }
 
     private void OnTriggerEnter(Collider other) {
