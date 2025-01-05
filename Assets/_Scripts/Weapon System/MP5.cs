a170d3bcc8978de5d8fa56d3cd1c1fde03471c07
IvanSeagull <ivan.seagull.k@gmail.com>
2025-01-05 18:57:23 +0000

    fix

--- Assets/_Scripts/Weapon System/MP5.cs
+++ Assets/_Scripts/Weapon System/MP5.cs
@@ -3,23 +3,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MP5 : Gun
-{
-    public override void Shoot()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, Mathf.Infinity))
-        { 
-            EnemyHealthController enemyHealthController = hit.transform.GetComponent<EnemyHealthController>();
-            if (enemyHealthController != null)
-            {
-                soundController.Play(soundController.hit);
-                enemyHealthController.TakeDamage(gunData.damage);
-            } else
-            {
-                GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
-                Destroy(impact, 0.5f);
-            }
-        }
-    }
-}
+public class MP5 : Gun {}
