78aa6e36be062d1d56ce2f5b85ea91b1a6142930
Kenji Berry <120066663+kenji-berry@users.noreply.github.com>
2025-01-06 00:16:26 +0000

    Added Voicelines for story

--- Assets/_Scripts/Story/VoiceLineTypewriter.cs
+++ Assets/_Scripts/Story/VoiceLineTypewriter.cs
@@ -23,6 +23,7 @@ public class VoiceLineTypewriter : MonoBehaviour
     private Queue<int> voiceLineQueue = new Queue<int>();
     public bool isPlaying;
     private Coroutine currentCoroutine;
+    private static int currentIndex = 0;
 
     private void Awake()
     {
@@ -31,23 +32,78 @@ public class VoiceLineTypewriter : MonoBehaviour
         
         voiceLines = new string[]
         {    
-            "System... malfunction?",
-            "This isn't the research lab.",
-            "Last thing I remember... the experiment. The teleporter. Something went wrong. Very wrong",
+            "Ugh... my head... what just happened?",
+            "This isn't the research lab... Where am I?",
+            "I was working on that strange device and then... everything went black.",
+            "If their technology brought me here... there has to be more transporters. A way back to Earth.",
+
+            "A teleporter? It looks functional... maybe it’s my way home.",  
+
+            "This place... it's like nothing I've ever seen before.",
+            "The carvings on these walls... they match the patterns on the artifact. This can't be a coincidence.", 
+            "This is space? I'm in space? How is that even possible?",
+            "I'm breathing... atmosphere control? They were prepared for biological life.",
+
+            "Lava? How is there lava in space?",
+            "This place is a death trap. I need to find a way out.",
+            "These look like the bounce pads we were working on back at the lab. How are they here?",
+            "Looks like they've been modified to work in space. I should be able to use them to get around.",
+            "I think I'll need to reach that button to open the door.",
+            "Another teleporter, second time's the charm?",
+
+            "I guess not...",
+            "How long will this go on for?",
+            "I need to find a way out of here... and talk to Micheal about this. He’ll know what’s going on.",
+            "If I can make it out...",
+            "Is this some kind of sick trial? Otherwise, why would they need to lay it out like this?",
+            "Those nightmares I was having... did they mean something?",
+            "Another teleporter, third time's the charm? For real this time.",
+
+            "This... this is the place I saw in my nightmares.",
+            "The people were alive though and they were... working.",
+            "Why can't I remember what they were working on?",
+            "...Please... let me go back.",
+
+            "That teleporter... it's different, its linked to earth!",
+            "This looks like some kind of sick ritual.",
+            "I just have to reach the top then I can go home.",
+
+            "My eyes... am I back? Why can’t I see anything?",
+            "These noises... I've heard them before, I must be back!",
+
+            "Johnny, your trial is complete.",
+            "Huh?... What trial? Who are you?",
+            "Micheal? Is that you? What's going on?",
+
+            "It’s me, yes... but it’s not what you think. This is the end of the cycle.",
+            "You created this. Your experiments. You went too far, and it twisted everything.",
+            "The mothership... it was your doing. Everything we’ve been through... it’s all part of the madness you unleashed." ,
+
+            "No... no, that can’t be right... I was trying to help.",
+
+            "Dont you feel like you've been here before? You have. You've been here many times. We both have.",
+            "This time I will end this finally wretched cycle.",
+
+            "No, please... I can fix this. I can fix everyth-",
+            "You can't fix this. You've tried. You've failed",
+            
+
+            "May you rest in peace, Johnny my best friend and greatest enemy.",
         };
     }
 
-    public void TriggerLine(int index)
+    public void TriggerLine()
     {
-        Debug.Log($"Queueing line {index}");
+        Debug.Log($"Queueing line {currentIndex}");
         
-        if (index < 0 || index >= voiceLines.Length)
+        if (currentIndex < 0 || currentIndex >= voiceLines.Length)
         {
-            Debug.LogError($"Invalid voice line index: {index}");
+            Debug.LogError($"Invalid voice line index: {currentIndex}");
             return;
         }
 
-        voiceLineQueue.Enqueue(index);
+        voiceLineQueue.Enqueue(currentIndex);
+        currentIndex++;
         
         // If not already playing, start processing the queue
         if (!isPlaying)
--- Assets/_Scripts/Story/VoiceTrigger.cs
+++ Assets/_Scripts/Story/VoiceTrigger.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class VoiceTrigger : MonoBehaviour
 {
     [SerializeField] private VoiceLineTypewriter typewriter;
-    [SerializeField] private int lineIndex;
     private bool hasTriggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (!hasTriggered && other.CompareTag("Player"))
         {
-            typewriter.TriggerLine(lineIndex);
+            typewriter.TriggerLine();
             hasTriggered = true;
             gameObject.SetActive(false);
         }
